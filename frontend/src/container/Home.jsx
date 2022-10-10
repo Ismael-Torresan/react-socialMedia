531cb27f22384780496c013bdfa9bac826164886
Ismael <ismaeltorresan1@gmail.com>
2022-10-10 11:32:06 -0300

    login ready

--- frontend/src/container/Home.jsx
+++ frontend/src/container/Home.jsx
@@ -1,4 +1,10 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { HiMenu } from "react-icons/hi";
+import { AiFillCloseCircle } from "react-icons/ai";
+import Sidebar from "../components/Sidebar";
+import { UserProfile, UserProfile } from "../components";
+import { client } from "../client";
+import logo from "../assets/logo.png";
 
 const Home = () => {
   return <div>Home</div>;
